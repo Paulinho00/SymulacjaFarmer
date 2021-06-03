a2cdd6e4d42172c1e05d3204640fc3a7cfa2aaec
Paweł Gryglewicz <259136@student.pwr.edu.pl>
2021-06-03 14:51:48 +0200

    Usuniecie bledu przy modyfikatorze dostepu w metodzie w planszy

--- src/main/java/miejsceSymulacji/Plansza.java
+++ src/main/java/miejsceSymulacji/Plansza.java
@@ -120,7 +120,7 @@ public class Plansza {
      * zwraca ilosc zajetych pol
      * @return {@link Plansza#zajetePola}
      */
-    private int getZajetePola() {
+    public int getZajetePola() {
         return zajetePola;
     }
 
